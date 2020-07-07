03decb78009e62c1e8a070db7f12181a2ee8b6d8
Chebuu <sethcbau@gmail.com>
2020-07-07 15:28:21 -0600

    completed Q8GTB6 annotations

--- R/sysdata.R
+++ R/sysdata.R
@@ -97,8 +97,8 @@ for(i in 1:length(GENETIC_CODE)) {
 }
 names(CANSAT_GENETIC_CODE) <- CANSAT_GENETIC_CODE_NAMES
 
-#' @name
-#' @title Dataset: \code{}
+#' @name ANNOTATIONS_Q8GTB6
+#' @title Dataset: \code{ANNOTATIONS_Q8GTB6}
 #' @description
 #' @format A list
 #' @source
@@ -106,21 +106,137 @@ names(CANSAT_GENETIC_CODE) <- CANSAT_GENETIC_CODE_NAMES
 ANNOTATIONS_Q8GTB6 <- list(
   'Sites' = list(
     'FAD-binding' = list(
-      desciption = 'FAD-binding sites',
-      ranges = IRangesList(
-        'H292' = IRanges(292),
-        'C417' = IRanges(417),
+      'H292' = list(
+        description = 'FAD C-N linkage',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges(292)
+        )
+      ),
+      'C417' = list(
+        description = 'FAD C-S linkage',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges(417)
+        )
       ),
     ),
     'Active' = list(
-      description = 'Catalytic residues',
-      ranges = IRangesList(
-        'Y484' = IRanges(484)
+      'Y484' = list(
+        description = 'Catalytic base Tyr484',
+        ranges = IRangesList(
+          'Y484' = IRanges(484)
+        )
+      )
+    ),
+    'Glycosylation' = list(
+      N65	 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N65',
+        label = 'GlcNAc-N65',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(N65)
+        )
+      ),
+      N89	 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N89',
+        label = 'GlcNAc-N89',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(N89)
+        )
+      ),
+      N168 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N168',
+        label = 'GlcNAc-N168',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(168)
+        )
+      ),
+      N297 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N297',
+        label = 'GlcNAc-N297',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(297)
+        )
+      ),
+      N305 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N305',
+        label = 'GlcNAc-N305',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(305)
+        )
+      ),
+      N329 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N329',
+        label = 'GlcNAc-N329',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(329)
+        )
+      ),
+      N467 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N467',
+        label = 'GlcNAc-N467',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(467)
+        )
+      ),
+      N499 = list(
+        type = 'GlNAc',
+        description = 'Glycosyl linkage GlNAc-N499',
+        label = 'GlcNAc-N499',
+        ranges = IRanges::IRangesList(
+          coordinates = IRanges::IRanges(499)
+        )
+      )
+    )
+  ),
+  'Tertiary' = list(
+    'Disulfide-bond' = list(
+      S99 = list(
+        description = 'Disulfide bond S37-S99',
+        label = 'S37-S99',
+        ranges = IRanges::IRanges(
+          coodinates = IRanges::IRanges(37),
+          linkcoords = IRanges::IRanges(99)
+        )
+      ),
+      S99 = list(
+        description = 'Disulfide bond S37-S99',
+        label = 'S37-S99',
+        ranges = IRanges::IRanges(
+          coodinates = IRanges::IRanges(37),
+          linkcoords = IRanges::IRanges(99)
+        )
+      )
+    ),
+    'Cross-link' = list(
+      H114 = list(
+        type = NULL,
+        description = 'Crsslink H114-[FAD]-C176',
+        label = 'H114-[FAD]-C176'
+        ranges = IRanges::IRanges(
+          coodinates = IRanges::IRanges(114),
+          linkcoords = IRanges::IRanges(176)
+        )
+      ),
+      C176 = list(
+        type = NULL,
+        description = 'Crosslink His-[FAD]-Cys',
+        label = 'H114-[FAD]-C176'
+        ranges = IRanges::IRanges(
+          coodinates = IRanges::IRanges(176),
+          coodinates = IRanges::IRanges(114)
+        )
       )
     )
   ),
   'Domains' = list(
     'FAD-binding' = list(
+      label = 'FAD-binding 80-219'
       desciption = 'FAD-binding domain',
       ranges = IRanges::IRangesList(
         coordinates = IRange(81, 219),
@@ -128,63 +244,118 @@ ANNOTATIONS_Q8GTB6 <- list(
       )
     ),
     'BBE-domain' = list(
+      label = 'BBE-domain 480-438',
       description = 'Berberine bidge-like domain',
       ranges = IRanges::IRangesList(
         coordinates = IRange(480, 438),
         aligncoords = IRange(480, 538),
       )
+    ),
+    'Signal-peptide' = list(
+      label = 'Signal-peptide 1-28',
+      description = 'Signal peptide',
+      ranges = IRanges::IRangesList(
+        coordinates = IRange(1, 28)
+      )
+    ),
+  ),
+  'Families' = list(
+    'FAD-binding' = list(
+      type = 'PCMH-type',
+      label = 'FAD-binding',
+      description = 'FAD-binding PCMH-type',
+      ranges = IRanges::IRangesList(
+        coordinates = IRanges::IRange(77, 251)
+      )
+    )
+  ),
+  'Composition' = list(
+    'Bias' = list(
+      type = 'compositional-bias',
+      label = 'Compositional bias 77-251',
+      description = 'Compositional bias 77-251',
+      ranges = IRanges::IRangesList(
+        coordinates = IRanges::IRange(77, 251)
+      )
+    )
+  ),
+  'Chains' = list(
+    'A' = list(
+      label = 'Chain-A 29-545'
+      dscription = 'Main chain'
+      ranges = IRanges::IRangeList(
+        coordinates = IRanges::IRange(29, 545)
+      )
     )
   ),
   'Mutations' = list(
     H92A = list(
+      effect = FALSE,
+      magnitude = '0',
       desciption = 'No effect',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     R108A = list(
+      effect = 'Kcat',
+      magnitude = '---',
       desciption = 'Strongly reduced catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     R110A = list(
+      effect = 'Kcat',
+      magnitude = '----',
       desciption = 'Strongly reduced catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     H114A = list(
+      effect = 'FAD',
+      magnitude = '-----',
       desciption = 'Loss of FAD binding and loss of catalytic activity. 2 Publications',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     H208A = list(
+      effect = FALSE,
+      magnitude = '0',
       desciption = 'No effect',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     H292A = list(
+      effect = 'Kcat',
+      magnitude = '----',
       desciption = 'Strongly reduced catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     Y417F = list(
+      effect = 'Kcat',
+      magnitude = '---',
       desciption = 'Reduced catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     E442Q = list(
+      effect = 'Kcat',
+      magnitude = '-',
       desciption = 'Slightly reduced catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
       )
     ),
     Y484F = list(
+      effect = 'Kcat',
+      magnitude = '-----',
       desciption = 'Loss of catalytic activity',
       ranges = IRanges::IRangesList(
         coodinates = IRanges::IRanges()
@@ -193,6 +364,59 @@ ANNOTATIONS_Q8GTB6 <- list(
   )
 )
 
-?Biostrings::IRangesList
-IRange(292)
-IRanges::IRanges(29)
+#' @name KINETICS
+#' @title Dataset: \code{KINETICS_Q8GTB6}
+#' @description
+#' @format A list
+#' @source
+#'\url{https://pfam.xfam.org/structure/3vte#tabview=tab0}
+KINETICS_Q8GTB6 <- list(
+  Kcat = list(
+    CBGA = list(
+      value = '0.30',
+      units = '1/sec'
+      label = 'Kcat[CBGA] = 0.30 sec(-1)',
+      description = 'Kcat value for CBGA substrate 0.30 sec(-1)'
+      references = c(NULL)
+    )
+  ),
+  Km = list(
+    CBGA = list(
+      value = c('134', '254'),
+      units = 'µM'
+      label = 'Km[CBGA] = (134 µM, 254 µM)',
+      description = 'Km values for CBGA (134 µM, 254 µM)'
+      references = c(
+        'https://doi.org/10.1074/jbc.M403693200',
+        'https://doi.org/10.1021/ja00143a024'
+      )
+    )
+  ),
+  Vmax = list(
+    CBGA = list(
+      value = c('0.36', '2.68'),
+      units = 'nmol/sec/mg',
+      label = 'Vmax[CBGA] = 2.68 nmol/sec/mg',
+      desciption = 'Vmax values for CBGA substrate (0.36 nmol/sec/mg, 2.68 nmol/sec/mg)',
+      references = c(
+        'https://doi.org/10.1074/jbc.M403693200',
+        'https://doi.org/10.1021/ja00143a024'
+      )
+    ),
+  ),
+  pH = list(
+    CBGA = list(
+      value = c('5.5', '6.0'),
+      units = 'pH',
+      label = 'Optimum pH 5.5-6.0',
+      desciption = 'Optimum catalytic pH for CBGA substrate between 5.5-6.0',
+      references = c(
+        'https://doi.org/10.1074/jbc.M403693200',
+        'https://doi.org/10.1021/ja00143a024'
+      )
+    )
+  ),
+)
+
+  
+  
